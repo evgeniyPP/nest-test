fec6d855a4a2114a07ec839a2d93c43bb5f4240a
Eugene Petryankin <epetryankin@devvela.com>
2021-10-10 20:26:59 +0300

    Add user create & findAll

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { User } from './users.model';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto): Promise<User> {
+    return this.userService.create(dto);
+  }
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+
+import { User } from './users.model';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User) private userRepo: typeof User) {}
+
+  async create(dto: CreateUserDto): Promise<User> {
+    return await this.userRepo.create(dto);
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepo.findAll();
+  }
+}
