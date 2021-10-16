312d853850527b281bd6c9b2ab47391bf741ac1b
Eugene Petryankin <epetryankin@devvela.com>
2021-10-16 18:59:15 +0300

    Prepare for push

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Post,
-  UseGuards,
-  UsePipes,
-} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import {
   ApiBearerAuth,
   ApiOperation,
